b62303b5fa1fdf5e24a200d9a966334d816be0a2
ginabeki <ginabeki3@gmail.com>
2023-01-07 16:11:02 +0300

    Update entry point with redux store

--- src/index.js
+++ src/index.js
@@ -1,34 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import App from './App';
 import store from './redux/configureStore';
-import Search from './pages/Search';
-import ErrorPage from './pages/ErrorPage';
-import Details from './pages/Details/Details';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: 'search',
-    element: <Search />,
-  },
-  {
-    path: 'search/details',
-    element: <Details />,
-  },
-]);
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
 root.render(
-  <Provider store={store}>
-    <RouterProvider router={router} />
-  </Provider>,
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
 );
